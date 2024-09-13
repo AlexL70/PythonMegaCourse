2abc4d4eb99db100a9681d9c10e170302308b909
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-09-13 07:35:27 +0300

    20. Getting User Input Repeatedly #while-loop

--- app1/main.py
+++ app1/main.py
@@ -1,11 +1,6 @@
 user_prompt = "Enter a todo: "
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
-todos = [todo1, todo2, todo3]
-
-print(todos)
-
-print(type(todos))  # array
-print(type(user_prompt))  # str
-print(type(todo1))  # str
+todos = []
+while True:
+    todo = input(user_prompt)
+    print("You entered:", todo)
+    print("Next...")
