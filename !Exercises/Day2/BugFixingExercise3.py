cd8812a31c120c0adacff24aed9cd3b95253c646
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-09-13 08:24:17 +0300

    27. Bug-Fixing Exercises

---
+++ !Exercises/Day2/BugFixingExercise3.py
@@ -0,0 +1,12 @@
+#countries = []
+#
+#while True:
+#    country = input("Enter the country: ")
+#    countries.append(country)
+#print(countries)
+
+countries = []
+while True:
+    country = input("Enter the country: ")
+    countries.append(country)
+    print(countries)
