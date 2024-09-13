781dcf7d67f06803e56b220ecf640ffd4b77d7e0
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-09-13 09:57:17 +0300

    33. Improving the Program Output #for-loops

--- app1/main.py
+++ app1/main.py
@@ -2,12 +2,16 @@ user_prompt = "Type add, show or exit: "
 todos = []
 while True:
     userAction = input(user_prompt)
-    match userAction:
+    match userAction.strip():
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
-        case "show":
-            print(todos)
+        case "show" | "display":
+            for item in todos:
+                item = item.title()
+                print(item)
         case "exit":
             break
+        case _unknown:
+            print("Unknown command:", _unknown)
 print("Good bye!")
