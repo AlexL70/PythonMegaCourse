7e71029cf67dae5fe30b74ebf8f7d7a0fd9e7ac6
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-09-19 12:35:32 +0300

    69. ✔️ Bug-Fixing Exercises

---
+++ !Exercises/Day6/BugFixingExercise1.py
@@ -0,0 +1,13 @@
+# file = open("data.txt", 'w')
+#
+# file.write("100.12")
+# file.write("111.23")
+#
+# file.close()
+
+file = open("data.txt", 'w')
+
+file.write("10012\n")
+file.write("111.23\n")
+
+file.close()
