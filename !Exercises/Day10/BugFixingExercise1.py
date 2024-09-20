e74c132baa3639994ce5b89d16e0a0672400c1e0
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-09-20 16:31:34 +0300

    104. ✔️ Bug-Fixing Exercises

---
+++ !Exercises/Day10/BugFixingExercise1.py
@@ -0,0 +1,13 @@
+# waiting_list = ["john", "marry"]
+# name = input("Enter name: ")
+#
+# number = waiting_list.index(name)
+# print(f"{name}'s turn is {number}")
+
+waiting_list = ["john", "marry"]
+name = input("Enter name: ")
+try:
+    number = waiting_list.index(name)
+    print(f"{name}'s turn is {number}")
+except ValueError:
+    print(f"{name} is not in tne list")
