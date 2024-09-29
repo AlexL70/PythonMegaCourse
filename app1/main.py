a93e857b36528f31035de858147b216fdaefab76
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-09-29 17:47:01 +0300

    115. Optimising the Code

--- app1/main.py
+++ app1/main.py
@@ -1,22 +1,23 @@
-def get_todos() -> list[str]:
-    with open("todos.txt", "r") as file:
+def get_todos(filePath) -> list[str]:
+    with open(filePath, "r") as file:
         todo_list = file.readlines()
     return [todo_el.strip('\n') for todo_el in todo_list] # remove linebreaks from todos
 
 
-def write_todos(todo_list: list[str]):
-    with open("todos.txt", 'w') as file:
+def write_todos(todo_list: list[str], filePath):
+    with open(filePath, 'w') as file:
         file.writelines([f"{todo_el}\n" for todo_el in todo_list])
 
+FILE_PATH = 'todos.txt'
 
 user_prompt = "Type add/new <todo>, edit <number>, show, complete <number> or exit: "
-todos = get_todos()
+todos = get_todos(FILE_PATH)
 while True:
     userAction = input(user_prompt).strip()
     if userAction.startswith( "add") or userAction.startswith("new"):
         todo = userAction[3:].strip()
         todos.append(f"{todo}")
-        write_todos(todos)
+        write_todos(todos, FILE_PATH)
     elif userAction == "show":
         for i, item in enumerate(todos):
             print(f"{i+1}. — {item}")
@@ -27,7 +28,7 @@ while True:
             print(f"You are editing \"{todos[num]}\"")
             newTodo = input("Enter a new value for the todo: ").strip()
             todos[num] = newTodo
-            write_todos(todos)
+            write_todos(todos, FILE_PATH)
         except (ValueError, IndexError):
             print("Bad command. After \"edit\" you should enter non-negative number from the list.")
             continue
@@ -37,7 +38,7 @@ while True:
             num = int(strNum) - 1
             deleted = todos.pop(num)
             print(f"\"{deleted}\" todo has been deleted.")
-            write_todos(todos)
+            write_todos(todos, FILE_PATH)
         except (ValueError, IndexError):
             print("Bad command. After \"complete\" you should enter non-negative number from the list.")
             continue
