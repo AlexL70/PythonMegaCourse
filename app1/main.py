baaf621e4a84a9909d12077c756da8361c69bc56
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-09-30 19:25:38 +0300

    124. Optimising the Code

--- app1/main.py
+++ app1/main.py
@@ -1,23 +1,24 @@
-def get_todos(filePath) -> list[str]:
+FILE_PATH = 'todos.txt'
+
+def get_todos(filePath=FILE_PATH) -> list[str]:
     with open(filePath, "r") as file:
         todo_list = file.readlines()
     return [todo_el.strip('\n') for todo_el in todo_list] # remove linebreaks from todos
 
 
-def write_todos(todo_list: list[str], filePath):
+def write_todos(todo_list: list[str], filePath=FILE_PATH):
     with open(filePath, 'w') as file:
         file.writelines([f"{todo_el}\n" for todo_el in todo_list])
 
-FILE_PATH = 'todos.txt'
 
 user_prompt = "Type add/new <todo>, edit <number>, show, complete <number> or exit: "
-todos = get_todos(FILE_PATH)
+todos = get_todos()
 while True:
     userAction = input(user_prompt).strip()
     if userAction.startswith( "add") or userAction.startswith("new"):
         todo = userAction[3:].strip()
         todos.append(f"{todo}")
-        write_todos(todos, FILE_PATH)
+        write_todos(todos)
     elif userAction == "show":
         for i, item in enumerate(todos):
             print(f"{i+1}. — {item}")
@@ -28,7 +29,7 @@ while True:
             print(f"You are editing \"{todos[num]}\"")
             newTodo = input("Enter a new value for the todo: ").strip()
             todos[num] = newTodo
-            write_todos(todos, FILE_PATH)
+            write_todos(todos)
         except (ValueError, IndexError):
             print("Bad command. After \"edit\" you should enter non-negative number from the list.")
             continue
@@ -38,7 +39,7 @@ while True:
             num = int(strNum) - 1
             deleted = todos.pop(num)
             print(f"\"{deleted}\" todo has been deleted.")
-            write_todos(todos, FILE_PATH)
+            write_todos(todos)
         except (ValueError, IndexError):
             print("Bad command. After \"complete\" you should enter non-negative number from the list.")
             continue
