89140410753c2930e5a1937c38acf701633b9db1
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-01 17:42:00 +0300

    135. Code Experiments

--- app1/modules/functions.py
+++ app1/modules/functions.py
@@ -11,3 +11,9 @@ def write_todos(todo_list: list[str], filePath=FILE_PATH):
     """ Saves todo list passed in todo_list parameter to the file """
     with open(filePath, 'w') as file:
         file.writelines([f"{todo_el}\n" for todo_el in todo_list])
+
+# Printen always
+print(__name__)
+# Printed only when THIS file is run; does not run when it is imported
+if __name__ == "__main__":
+    print("Hello from functions!")
