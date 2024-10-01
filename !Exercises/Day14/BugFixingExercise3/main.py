5229cf69a7dadd9a5c9272e3a4fb7167b45af74b
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-01 18:30:57 +0300

    137. Bug-Fixing Exercises

---
+++ !Exercises/Day14/BugFixingExercise3/main.py
@@ -0,0 +1,9 @@
+# from functions import count
+#
+# nr_of_periods = functions.count("trees are good. grass is green.")
+# print(nr_of_periods)
+
+from functions import count
+
+nr_of_periods = count("trees are good. grass is green.")
+print(nr_of_periods)
