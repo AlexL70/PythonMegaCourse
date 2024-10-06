011cfd1dbec0d4c547b9f50c93b4103afddfd4c7
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-06 17:46:23 +0300

    162. Implementing an "Add Todo" Button

--- app1/gui.py
+++ app1/gui.py
@@ -1,9 +1,28 @@
-import functools
+import modules.functions as fn
 import FreeSimpleGUI as sg
 
+
+"""
+Keys used to identify controls
+"""
+TODO_INPUT: str="todo_input"
+ADD_BTN: str="add_btn"
+
 label = sg.Text("Type in a to-do:")
-input_box = sg.InputText(tooltip="Please enter a to-do")
-add_button = sg.Button("Add")
-wnd = sg.Window('My To-Do App', layout=[[label],[input_box, add_button]])
-wnd.read()
+input_box = sg.InputText(tooltip="Please enter a to-do", key=TODO_INPUT)
+add_button = sg.Button("Add", key=ADD_BTN)
+wnd = sg.Window('My To-Do App',
+                layout=[[label],[input_box, add_button]],
+                font=('Helvetica', 18))
+
+todos = fn.get_todos()
+while True:
+    event, values = wnd.read()
+    match event:
+       case sg.WIN_CLOSED:
+           break
+       case ADD_BTN:
+           todos.append(values[TODO_INPUT])
+           fn.write_todos(todos)
+
 wnd.close()
