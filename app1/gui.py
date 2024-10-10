40c037329e9bb66878ca926fbe054eaff9535475
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-10 17:22:32 +0300

    172. Implement Complete and Exit Buttons

--- app1/gui.py
+++ app1/gui.py
@@ -8,7 +8,9 @@ class ControlKeys:
     TODO_INPUT: str="__todo_input__"
     TODO_LST: str="__todo_lst__"
     ADD_BTN: str="__add_btn__"
-    EDIT_BTN: str="__edit_bnt__"
+    EDIT_BTN: str="__edit_btn__"
+    COMPLETE_BTN: str="__complete_btn__"
+    EXIT_BTN: str="__exit_btn__"
 
 # Read todo list from the file
 todos = fn.get_todos()
@@ -17,17 +19,20 @@ label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Please enter a to-do", key=ControlKeys.TODO_INPUT)
 add_button = sg.Button("Add", key=ControlKeys.ADD_BTN)
 edit_button = sg.Button("Edit", key=ControlKeys.EDIT_BTN)
+complete_button = sg.Button("Complete", key=ControlKeys.COMPLETE_BTN)
+exit_button = sg.Button("Exit", key=ControlKeys.EXIT_BTN)
 todo_list = sg.Listbox(values=todos, key=ControlKeys.TODO_LST, select_mode=sg.LISTBOX_SELECT_MODE_SINGLE, bind_return_key=True, auto_size_text=True, enable_events=True, size=(40,10))
 wnd = sg.Window('My To-Do App',
                 layout=[[label],
                         [input_box, add_button],
-                        [todo_list, edit_button]],
+                        [todo_list, edit_button, complete_button],
+                        [exit_button]],
                 font=('Helvetica', 18))
 
 while True:
     event, values = wnd.read()
-    print(event)
-    print(values)
+    #print(event)
+    #print(values)
     match event:
         case ControlKeys.ADD_BTN:
             todos.append(values[ControlKeys.TODO_INPUT])
@@ -40,9 +45,19 @@ while True:
             todos[index] = edit_line
             fn.write_todos(todos)
             wnd[ControlKeys.TODO_LST].update(values=todos)
+        case ControlKeys.COMPLETE_BTN:
+            list_item = values[ControlKeys.TODO_LST][0]
+            index = todos.index(list_item)
+            todos.pop(index)
+            fn.write_todos(todos)
+            wnd[ControlKeys.TODO_LST].update(values=todos)
+            list_item = values[ControlKeys.TODO_LST][0]
+            wnd[ControlKeys.TODO_INPUT].update(value='')
         case ControlKeys.TODO_LST:
             list_item = values[ControlKeys.TODO_LST][0]
             wnd[ControlKeys.TODO_INPUT].update(value=list_item)
+        case ControlKeys.EXIT_BTN:
+            break
         case sg.WIN_CLOSED:
             break
 
