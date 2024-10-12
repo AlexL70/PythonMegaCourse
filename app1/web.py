698203c5cf2ed5980b04facd79458cc3fb030c5b
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-12 13:05:57 +0300

    183. Adding New Todo Items on the Web App

--- app1/web.py
+++ app1/web.py
@@ -1,14 +1,37 @@
 import streamlit as st
 import modules.functions as fn
 
+class ControlKeys:
+    TODO_INPUT: str = "__todo_input__"
+    EDIT_BTN: str = "__edit_button__"
+    COMPLETE_BTN: str = "__complete__button"
+    TODO_ITEM: str = "__todo__item__"
+
+
 todos = fn.get_todos()
+
+def add_todo():
+    item = st.session_state[ControlKeys.TODO_INPUT]
+    todos.append(item)
+    fn.write_todos(todos)
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.text("This app is to increase you productivity.")
 
+col1, col2 = st.columns((4,1))
+
 for index, todo in enumerate(todos):
-    st.checkbox(f"{index + 1} – {todo}")
+    row = st.columns((4,1,1))
+    with st.container():
+        with row[0]:
+            st.write(f"{index + 1} – {todo}")
+        with row[1]:
+            st.button("Edit", key=f"{ControlKeys.EDIT_BTN}{index}", use_container_width=True)
+        with row[2]:
+            st.button("Complete", key=f"{ControlKeys.COMPLETE_BTN}{index}", use_container_width=True)
 
-st.text_input(label="Enter a todo to be added:", placeholder="New todo")
 
+st.text_input(label="Enter a todo to be added:", placeholder="New todo", on_change=add_todo, key=ControlKeys.TODO_INPUT)
+
+print(st.session_state)
