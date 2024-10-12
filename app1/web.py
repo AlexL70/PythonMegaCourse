a1f077f04ba793fa4905109dfa76ffd33b3da92c
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-12 15:53:30 +0300

    (Mine) Editint Todo Items on the Web App

--- app1/web.py
+++ app1/web.py
@@ -6,13 +6,25 @@ class ControlKeys:
     EDIT_BTN: str = "__edit_button__"
     COMPLETE_BTN: str = "__complete__button"
     TODO_ITEM: str = "__todo__item__"
+    EDIT_INDEX: str = "__edit__index__"
 
 
 todos = fn.get_todos()
 
-def add_todo():
+try:
+    edit_index = st.session_state[ControlKeys.EDIT_INDEX]
+except KeyError:
+    edit_index = None
+
+def add_or_edit_todo():
     item = st.session_state[ControlKeys.TODO_INPUT]
-    todos.append(item)
+    if item is None or len(item) == 0:
+        return
+    if edit_index is None:
+        todos.append(item)
+    else:
+        todos[edit_index] = item
+        st.session_state[ControlKeys.EDIT_INDEX] = None
     fn.write_todos(todos)
     st.session_state[ControlKeys.TODO_INPUT] = None
 
@@ -24,6 +36,16 @@ def complete_todo():
             fn.write_todos(todos)
             break
 
+def edit_mode():
+    for i, item in enumerate(todos):
+        edited = st.session_state[f"{ControlKeys.EDIT_BTN}{i}"]
+        if edited and edit_index is None:
+            # Enter edit mode if "Edit" is clicked
+            st.session_state[ControlKeys.EDIT_INDEX] = i
+            st.session_state[ControlKeys.TODO_INPUT] = item
+            break
+
+
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.text("This app is to increase you productivity.")
@@ -32,15 +54,21 @@ col1, col2 = st.columns((4,1))
 
 for index, todo in enumerate(todos):
     row = st.columns((4,1,1))
+    disable_buttons = not edit_index is None
     with st.container():
         with row[0]:
-            st.write(f"{index + 1} – {todo}")
+            todo_str = f"{index + 1} – {todo}"
+            if edit_index == index:
+                st.write(f"{todo_str} :red[{'(edited)'}]")
+            else:
+                st.write(todo_str)
         with row[1]:
-            st.button("Edit", key=f"{ControlKeys.EDIT_BTN}{index}", use_container_width=True)
+            st.button("Edit", key=f"{ControlKeys.EDIT_BTN}{index}", use_container_width=True, on_click=edit_mode, disabled=disable_buttons)
         with row[2]:
-            st.button("Complete", key=f"{ControlKeys.COMPLETE_BTN}{index}", use_container_width=True, on_click=complete_todo)
+            st.button("Complete", key=f"{ControlKeys.COMPLETE_BTN}{index}", use_container_width=True, on_click=complete_todo, disabled=disable_buttons)
 
-
-st.text_input(label="Enter a todo to be added:", placeholder="New todo", on_change=add_todo, key=ControlKeys.TODO_INPUT)
-
-# print(st.session_state)
+if edit_index is None:
+    inp_lbl_text = "Enter a todo to be added:"
+else:
+    inp_lbl_text = f"Editing item #{edit_index + 1}"
+st.text_input(label=inp_lbl_text, placeholder="New todo", on_change=add_or_edit_todo, key=ControlKeys.TODO_INPUT)
