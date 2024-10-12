a72afa832825ed4d9b7af2b5e84cfc553dadd662
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-12 20:41:54 +0300

    188. Coding Exercise 1

--- app1/Bonus/bonus19.py
+++ app1/Bonus/bonus19.py
@@ -1,8 +1,14 @@
 import streamlit as st
 from PIL import Image as Img
+
+uploaded_image = st.file_uploader("Upload image.")
 with st.expander("Start camera."):
     cam_image = st.camera_input("Camera")
-if cam_image:
-    image = Img.open(cam_image)
+if cam_image or uploaded_image:
+    if cam_image:
+        image = Img.open(cam_image)
+    else:
+        image = Img.open(uploaded_image)
+        print(uploaded_image)
     gray_img = image.convert("L")
     st.image(gray_img)
