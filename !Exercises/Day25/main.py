525bed7f9ae0e8d6d95476c1b8aa57a193b6a48e
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-17 18:45:33 +0300

    238. Student Project: From Text Files to PDF

---
+++ !Exercises/Day25/main.py
@@ -0,0 +1,12 @@
+import os
+from fpdf import FPDF
+import pathlib as path
+
+pdf = FPDF(orientation="portrait", unit="mm", format="A4")
+for file_name in os.listdir("textfiles"):
+    if file_name.endswith(".txt"):
+        name = path.Path(f"textfiles/{file_name}").stem
+        pdf.add_page()
+        pdf.set_font("Times", size=16, style="B")
+        pdf.cell(w=200, h=16, txt=name.capitalize(), ln=True, align="L")
+pdf.output("output.pdf")
