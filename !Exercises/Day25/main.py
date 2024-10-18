3dab3be793b1419c34074fbff97b858ccae457cd
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-18 17:56:30 +0300

    246. Student Project: Adding Multiline Text to PDF

--- !Exercises/Day25/main.py
+++ !Exercises/Day25/main.py
@@ -9,4 +9,8 @@ for file_name in os.listdir("textfiles"):
         pdf.add_page()
         pdf.set_font("Times", size=16, style="B")
         pdf.cell(w=200, h=16, txt=name.capitalize(), ln=True, align="L")
+    with open(f"textfiles/{file_name}", "r") as file:
+        pdf.set_font("Times", size=12)
+        for line in file:
+            pdf.multi_cell(w=200, h=8, txt=line, align="L")
 pdf.output("output.pdf")
