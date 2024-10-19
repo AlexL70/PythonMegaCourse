2840204555fa42a603612ba3480bf9e8cfeb7d2d
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-19 19:07:34 +0300

    260. Student Project: Solution

--- !Exercises/Day28/main.py
+++ !Exercises/Day28/main.py
@@ -12,15 +12,18 @@ response = rq.get(url)
 if response.status_code != 200:
     print(f"Error: {response.status_code}: {response.text}")
     exit(1)
-image_url = response.json()["hdurl"]
+data = response.json()
+image_url = data["hdurl"]
+title = data["title"]
+text = data["explanation"]
 img_response = rq.get(image_url)
 with open("image.jpg", "wb") as file:
     file.write(img_response.content)
-text = response.json()["explanation"]
 
 st.set_page_config(page_title="NASA APOD", page_icon="🌌", layout="wide")
 st.title("NASA Astronomy Picture of the Day")
 st.subheader(today)
 st.write("This is not a NASA site. This is a Streamlit app that uses the NASA API. So the image and text are from NASA.")
+st.write(f"<h2>{title}</h2>", unsafe_allow_html=True)
 st.image("./image.jpg")
 st.write(text)
