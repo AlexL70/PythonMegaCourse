40e16c052b93e705f6e3b084d16a36b82269c6e3
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-20 20:03:06 +0300

    274. Student Project: Build a Thesaurus API (Part 2)

--- !Exercises/Day29/DictionaryApi/main.py
+++ !Exercises/Day29/DictionaryApi/main.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template
+import requests as rq
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/api/v1/<word>')
 def dictionary(word: str):
-    return {
-        'word': word,
-        'definition': word.upper()
-    }
+    url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+    result = {"definition": None, "word": word}
+    response = rq.get(url)
+    if response.status_code != 200:
+        return result
+    content = response.json()
+    definitions = content[0]["meanings"][0]["definitions"]
+    whole_def = ""
+    for index, definition in enumerate(definitions):
+        whole_def += f"{(index + 1)}. {definition['definition']}\n "
+    result["definition"] = whole_def
+    return result
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
